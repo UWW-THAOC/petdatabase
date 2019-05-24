8972dede9bd490e0eec5d8dde9b765606e7340cf
Cheng Thao <cheng.thao@ictect.com>
2019-05-24 14:05:48 -0500

    Add the PetDatabase class

---
+++ src/PetDatabase.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+
+
+
+public class PetDatabase {
+    
+    private ArrayList<Pet> pets = new ArrayList<Pet>();
+    
+    public void add(Pet p){
+        pets.add(p);
+    }
+    
+    public ArrayList<Pet> getPets(){
+        return pets;
+    }
+    
+    public int getCount(){
+        return pets.size();
+    }
+}
